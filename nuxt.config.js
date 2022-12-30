be3e2af79104d280a3cd39fc1de3cb4db5d36ded
Đỗ Hoàng Sơn <sondh18@viettel.com.vn>
2022-12-30 17:24:53 +0700

    v1

---
+++ nuxt.config.js
@@ -0,0 +1,118 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - keycloak-demo-app',
+    title: 'keycloak-demo-app',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    '@nuxtjs/auth-next',
+    '@nuxtjs/proxy'
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    proxy: true
+  },
+
+  proxy: {
+    '/auth': {
+      target: 'http://localhost:8080'
+    }
+  },
+
+  router: {
+    middleware: ['auth']
+  },
+
+  server: {
+    port: 4500,
+  },
+
+  auth: {
+    strategies: {
+      keycloak: {
+        scheme: 'openIDConnect',
+        endpoints: {
+          configuration: '/auth/realms/test/.well-known/openid-configuration'
+        },
+        clientId: 'keycloak-demo-app',
+        scope: ['openid', 'profile', 'offline_access'],
+        acrValues: "0",
+      },
+    },
+    watchLoggedIn: true,
+    resetOnError: true,
+    redirect: {
+      login: '/login',
+      logout: '/',
+      home: '/'
+    }
+  },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container fluid fill-height>
+    <v-flex xs12 sm8 md4>
+      <v-card class="elevation-12">
+        <v-btn @click="login()">
+          Login via Keycloak
+        </v-btn>
+      </v-card>
+    </v-flex>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'LoginVue',
+  layout: 'default',
+  methods: {
+    async login () {
+      try {
+        await this.$auth.loginWith('keycloak')
+        // eslint-disable-next-line
+        this.$router.push('/')
+      } catch (err) {
+        // eslint-disable-next-line
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
